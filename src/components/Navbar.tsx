2e55c6423bc9f58f0a3577f127c5378b4feaa9b9
Lynlcrz <117515423+Lynlcrz@users.noreply.github.com>
2025-05-08 22:56:17 +0800

    malapit na

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const sections = ["home", "about", "projects", "contact"];
+const sections = ["home", "skills", "projects", "contact"];
 
 type NavbarProps = React.HTMLAttributes<HTMLElement>;
 
